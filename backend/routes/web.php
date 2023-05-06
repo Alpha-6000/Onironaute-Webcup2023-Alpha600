0de8a32b43af6d41754a8ab1fff56b8730aafa1c
Mampionona <testeurr@gmail.com>
2023-05-06 17:46:18 +0300

    mamafa ilay route ana messagerie

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -23,10 +23,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('ChatBot600', ChatBot600Controller::class)
-    ->only(['index', 'store'])
-    ->middleware(['auth', 'verified']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
